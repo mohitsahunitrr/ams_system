bff851e26dc6e76a755ef9dc7f3cf87dcd88c103
Philip A Chapman <pchapman@pcsw.us>
2018-12-05 17:34:25 -0800

    Changed work order to be for multiple sites.

---
+++ ams-common/src/main/java/com/precisionhawk/ams/domain/SitesAware.java
@@ -0,0 +1,15 @@
+/*
+ * All rights reserved.
+ */
+package com.precisionhawk.ams.domain;
+
+import java.util.List;
+
+/**
+ *
+ * @author Philip A. Chapman
+ */
+public interface SitesAware {
+    
+    public List<String> getSiteIds();
+}
--- ams-common/src/main/java/com/precisionhawk/ams/domain/WorkOrder.java
+++ ams-common/src/main/java/com/precisionhawk/ams/domain/WorkOrder.java
@@ -6,6 +6,8 @@ package com.precisionhawk.ams.domain;
 
 import io.swagger.oas.annotations.media.Schema;
 import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -13,7 +15,7 @@ import java.util.Objects;
  * @author <a href="mailto:pchapman@pcsw.us">Philip A. Chapman</a>
  */
 @Schema(description="An order for work.")
-public class WorkOrder implements SiteAware {
+public class WorkOrder implements SitesAware {
 
     @Schema(description="A description of the work to be done.")
     private String description;
@@ -42,14 +44,14 @@ public class WorkOrder implements SiteAware {
         this.requestDate = requestDate;
     }
     
-    @Schema(description="The site at which the work is to be done.")
-    private String siteId;
+    @Schema(description="The sites at which the work is to be done.")
+    private List<String> siteIds = new LinkedList<>();
     @Override
-    public String getSiteId() {
-        return siteId;
+    public List<String> getSiteIds() {
+        return siteIds;
     }
-    public void setSiteId(String siteId) {
-        this.siteId = siteId;
+    public void setSiteIds(List<String> siteIds) {
+        this.siteIds = siteIds;
     }
 
     @Schema(description="The status of the work.")
