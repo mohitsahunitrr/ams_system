c3ec84224da2e610f525bcff233ae408f3dc5886
Philip A Chapman <pchapman@pcsw.us>
2019-01-15 13:11:27 -0800

    Added funcionality so that the yaml security file will be re-loaded if changed.

--- ams-dao/src/main/java/com/precisionhawk/ams/dao/fs/YamlFileSecurityDao.java
+++ ams-dao/src/main/java/com/precisionhawk/ams/dao/fs/YamlFileSecurityDao.java
@@ -9,6 +9,7 @@ import com.precisionhawk.ams.bean.security.UserSearchParams;
 import com.precisionhawk.ams.dao.DaoException;
 import com.precisionhawk.ams.dao.SimpleSecurityDao;
 import com.precisionhawk.ams.domain.Organization;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
@@ -17,6 +18,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,24 +28,45 @@ import org.slf4j.LoggerFactory;
  */
 public class YamlFileSecurityDao implements SimpleSecurityDao {
     
+    private final Object LOCK = new Object();
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
     
     private SecurityData data;
     
-    public void configure(String configFile) {
-        Reader reader = null;
-        try {
-            reader = new FileReader(configFile);
-            YamlReader yamlreader = new YamlReader(reader);
-            data = yamlreader.read(SecurityData.class);
-        } catch (IOException ex) {
-            LOGGER.error(String.format("Error loading security data from file %s", configFile), ex);
-            throw new RuntimeException("Error loading security data.");
+    private File configFile;
+    
+    private long lastModified = 0;
+    
+    public void configure(String configFilePath) {
+        configFile = new File(configFilePath);
+        ensureData(true);
+    }
+    
+    private void ensureData(boolean failSafe) {
+        synchronized (LOCK) {
+            if (lastModified != configFile.lastModified()) {
+                Reader reader = null;
+                try {
+                    reader = new FileReader(configFile);
+                    YamlReader yamlreader = new YamlReader(reader);
+                    SecurityData d = yamlreader.read(SecurityData.class);
+                    lastModified = configFile.lastModified();
+                    data = d;
+                } catch (IOException ex) {
+                    LOGGER.error(String.format("Error loading security data from file %s", configFile), ex);
+                    if (failSafe) {
+                        throw new RuntimeException("Error loading security data.");
+                    }
+                } finally {
+                    IOUtils.closeQuietly(reader);
+                }
+            }
         }
     }
     
     @Override
     public String getPassHash(String userId) {
+        ensureData(false);
         SelfContainedUserInfo ui = data.getUsers().get(userId);
         if (ui == null) {
             return null;
@@ -64,11 +87,13 @@ public class YamlFileSecurityDao implements SimpleSecurityDao {
 
     @Override
     public Application selectApplicationById(String appId) {
+        ensureData(false);
         return data.getApplications().get(appId);
     }
 
     @Override
     public List<Application> selectApplications() {
+        ensureData(false);
         List<Application> apps = new ArrayList<>(data.getApplications().values());
         return apps;
     }
@@ -97,6 +122,7 @@ public class YamlFileSecurityDao implements SimpleSecurityDao {
 
     @Override
     public List<Group> selectGroupsOrderedByName() {
+        ensureData(false);
         List<Group> groups = new ArrayList<>(data.getGroups().values());
         Collections.sort(groups, NAME_COMPARATOR);
         return groups;
@@ -104,6 +130,7 @@ public class YamlFileSecurityDao implements SimpleSecurityDao {
 
     @Override
     public List<Group> selectGroupsForAppOrderedByName(String appId) {
+        ensureData(false);
         List<Group> groups = new ArrayList<>();
         for (Group g : data.getGroups().values()) {
             if (g.getApplicationId().equals(appId)) {
@@ -116,6 +143,7 @@ public class YamlFileSecurityDao implements SimpleSecurityDao {
 
     @Override
     public Group selectGroupByKey(String appId, String key) {
+        ensureData(false);
         for (Group g : data.getGroups().values()) {
             if (g.getKey().equals(key)) {
                 return g;
@@ -136,16 +164,19 @@ public class YamlFileSecurityDao implements SimpleSecurityDao {
 
     @Override
     public Organization selectOrganizationById(String orgId) {
+        ensureData(false);
         return data.getOrganizations().get(orgId);
     }
 
     @Override
     public List<Organization> selectOrganizations() {
+        ensureData(false);
         return new ArrayList<>(data.getOrganizations().values());
     }
 
     @Override
     public List<Organization> selectOrganizationsForUser(String userId) {
+        ensureData(false);
         SelfContainedUserInfo ui = data.getUsers().get(userId);
         if (ui == null) {
             return Collections.emptyList();
@@ -170,6 +201,7 @@ public class YamlFileSecurityDao implements SimpleSecurityDao {
 
     @Override
     public Organization selectOrganizationByKey(String orgKey) {
+        ensureData(false);
         for (Organization org : data.getOrganizations().values()) {
             if (org.getKey().equals(orgKey)) {
                 return org;
@@ -180,6 +212,7 @@ public class YamlFileSecurityDao implements SimpleSecurityDao {
 
     @Override
     public List<String> selectSitesForUser(String userId) {
+        ensureData(false);
         SelfContainedUserInfo ui = data.getUsers().get(userId);
         if (ui == null) {
             return Collections.emptyList();
@@ -210,6 +243,7 @@ public class YamlFileSecurityDao implements SimpleSecurityDao {
 
     @Override
     public List<CachedUserInfo> selectUserInfo(UserSearchParams parameters) {
+        ensureData(false);
         // Only email address makes sense here.
         if (parameters.getEmailAddress() == null || parameters.getEmailAddress().isEmpty()) {
             return Collections.emptyList();
@@ -227,6 +261,5 @@ public class YamlFileSecurityDao implements SimpleSecurityDao {
     @Override
     public boolean updateUserInfo(UserInfoBean userInfo) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+    }    
 }
