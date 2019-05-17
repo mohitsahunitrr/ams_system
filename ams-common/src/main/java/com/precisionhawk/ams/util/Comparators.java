0687960d08957e7a2cce89ff2c2f604c5dfb3b05
Philip A Chapman <pchapman@pcsw.us>
2019-05-17 10:06:44 -0700

    Added some useful comparators.

--- ams-common/src/main/java/com/precisionhawk/ams/util/Comparators.java
+++ ams-common/src/main/java/com/precisionhawk/ams/util/Comparators.java
@@ -1,6 +1,9 @@
 package com.precisionhawk.ams.util;
 
+import com.precisionhawk.ams.domain.Organization;
 import com.precisionhawk.ams.domain.ResourceMetadata;
+import com.precisionhawk.ams.domain.Site;
+import com.precisionhawk.ams.domain.WorkOrder;
 import java.time.ZonedDateTime;
 import java.util.Comparator;
 
@@ -9,6 +12,26 @@ import java.util.Comparator;
  * @author pchapman
  */
 public interface Comparators {
+    
+    public static Comparator<Organization>  ORGS_COMPARATOR = new Comparator<Organization>() {
+        @Override
+        public int compare(Organization o1, Organization o2) {
+            String n1 = o1 == null ? null : o1.getName();
+            String n2 = o2 == null ? null : o2.getName();
+            if (n1 == null) {
+                if (n2 == null) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            } else if (n2 == null) {
+                return -1;
+            } else {
+                return n1.compareTo(n2);
+            }
+        }
+    };
+
     public static Comparator<ResourceMetadata> RESOURCE_BY_TIMESTAMP = new Comparator<ResourceMetadata>() {
         @Override
         public int compare(ResourceMetadata o1, ResourceMetadata o2) {
@@ -24,6 +47,25 @@ public interface Comparators {
                 return -1;
             } else {
                 return t1.compareTo(t2);
+            }
+        }
+    };
+    
+    public static Comparator<WorkOrder>  WORK_ORDERS_COMPARATOR = new Comparator<WorkOrder>() {
+        @Override
+        public int compare(WorkOrder wo1, WorkOrder wo2) {
+            String n1 = wo1 == null ? null : wo1.getOrderNumber();
+            String n2 = wo2 == null ? null : wo2.getOrderNumber();
+            if (n1 == null) {
+                if (n2 == null) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            } else if (n2 == null) {
+                return -1;
+            } else {
+                return n1.compareTo(n2);
             }
         }
     };
